b21fff094290d6db03f2cd8889f0873f24a7d503
YuriWit <yuridewitherran@gmail.com>
2022-06-14 20:13:07 -0300

    just locking at hex return to see

--- SerialHandle.py
+++ SerialHandle.py
@@ -1,6 +1,6 @@
 import serial
 from mnist import MNIST
-import random, struct
+import random, struct, binascii
 
 
 mndata = MNIST('samples')
@@ -10,6 +10,7 @@ images, labels = mndata.load_training()
 s = serial.Serial('COM6')
 s.timeout = 100
 # struct.pack("!f", 4.1)
+# a.append(int.from_bytes(res, "big"))
 def send(index):
     print("sending:", labels[index])
     data = b''
@@ -17,7 +18,6 @@ def send(index):
         #data += i.to_bytes(1, "big")
         data += struct.pack("!f", i/256)
     s.write(data)
-    print(len(data))
     a = b''
     for i in range(40):
         res = s.read()
@@ -27,12 +27,17 @@ def send(index):
     for i in range(10):
         f = struct.unpack('f', a[4*i:4*i+4])
         b.append(f)
-    for i in range(len(b)):
-        print(i, b[i])
+    #for i in range(len(b)):
+        #print(i, b[i])
+    return a
 
 def getNum(a):
     max_value = max(a)
     index = a.index(max_value)
     return index
 
+def p(a):
+    for i in range(10):
+        print(binascii.hexlify(bytearray(a[4*i:4*i+4])))
+    print()
 
