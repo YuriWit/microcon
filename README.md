ff913aee00cac771f35a638b9e618ce9122b749e
YuriWit <yuridewitherran@gmail.com>
2022-06-14 23:07:14 -0300

    added readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Embarcar IA em STM
+## Descricao
+Embarcar numa STM32 uma IA capaz de identificar os algarismos 
+
+## Funcao
+Adquirir conhecimento sobre IAs em sistemas embarcados
+
+## Motivacao
+Aplicacao da metodologia no projeto do humanoid da RoboIme
+
+##  Pinout
+
+| USB | PA12 | OTG_FS_DP            |
+|-----|------|----------------------|
+| USB | PA11 | OTG_FS_DM            |
+| USB | PA10 | OTG_FS_ID            |
+| USB | PA9  | VBUS_FS              |
+| USB | PC0  | OTG_FS_PowerSwitchOn |
+| USB | PD5  | OTG_FS_OverCurrent   |
--- SerialHandle.py
+++ SerialHandle.py
@@ -1,6 +1,7 @@
 import serial
 from mnist import MNIST
 import random, struct, binascii
+import numpy as np
 
 
 mndata = MNIST('samples')
@@ -12,11 +13,14 @@ s.timeout = 100
 # struct.pack("!f", 4.1)
 # a.append(int.from_bytes(res, "big"))
 def send(index):
-    print("sending:", labels[index])
+    #print("sending:", labels[index])
     data = b''
     for i in images[index]:
         #data += i.to_bytes(1, "big")
-        data += struct.pack("!f", i/256)
+        #val = i^255
+        val = i
+        data += struct.pack("!f", val/256)
+        #data += struct.pack("!i", i)
     s.write(data)
     a = b''
     for i in range(40):
@@ -24,11 +28,6 @@ def send(index):
         a+=res
     #print(a, len(a))
     b = []
-    for i in range(10):
-        f = struct.unpack('f', a[4*i:4*i+4])
-        b.append(f)
-    #for i in range(len(b)):
-        #print(i, b[i])
     return a
 
 def getNum(a):
@@ -36,8 +35,35 @@ def getNum(a):
     index = a.index(max_value)
     return index
 
-def p(a):
+def h(a):
     for i in range(10):
         print(binascii.hexlify(bytearray(a[4*i:4*i+4])))
     print()
 
+def f(a):
+    b = []
+    for i in range(10):
+        f = struct.unpack('f', a[4*i:4*i+4])
+        b.append(f)
+        #print(i,"{:.2f}".format(f[0]))
+    c = np.array(b)
+    return np.argmax(c)
+
+def test(i):
+    a = send(i)
+    ip = f(a)
+    ir = labels[i]
+    if ip==ir:
+        #print(i)
+        return True
+    return False
+
+def runStat():
+    a = 0
+    for i in range(60000):
+        if test(i):
+            a+=1
+            print(a/(i+1), end='\r')
+
+index = random.randrange(0, len(images))
+print(index, labels[index], f(send(index)))
